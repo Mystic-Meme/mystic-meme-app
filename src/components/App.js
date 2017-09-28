e6b39d120a91f7224fa9b280098fa728786d6fd6
Stephen Boynton <boyntonsc@gmail.com>
2017-09-28 11:04:50 -0400

    Fix App conflict

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import '../styles/App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
-    );
-  }
-=======
 import React, { Component } from "react";
 import Title from "./Title";
 import CrystalBallView from "./CrystalBallView";
@@ -27,5 +12,4 @@ class App extends Component {
 			</div>
 		);
 	}
->>>>>>> 88a81da9bf712808bb62a811d13eb80660e26222
 }
