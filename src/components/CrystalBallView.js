0322fee64da918d3490ed96eed749f9f45dfc940
Nick Nauert <nicknauert@gmail.com>
2017-10-01 14:03:27 -0400

    MeRgEd

--- src/components/CrystalBallView.js
+++ src/components/CrystalBallView.js
@@ -2,6 +2,7 @@ import React from "react";
 import CrystalBallImage from "./CrystalBallImage";
 import CrystalBallTextResponse from "./CrystalBallTextResponse";
 import BackgroundImage from "./BackgroundImage";
+import CrystalBallButton from "./CrystalBallButton"
 
 export default function CrystalBallView() {
 	return (
@@ -10,9 +11,9 @@ export default function CrystalBallView() {
 				You should know, not all is as it seems ... <br /> Now choose your
 				teller, and recieve you your meme ...
 			</h1>
-			<CrystalBallButton />
-			<CrystalBallButton />
-			<CrystalBallImage  class="CrystalBallImage"/>
+			{/* <CrystalBallButton /> */}
+			{/* <CrystalBallButton /> */}
+			<CrystalBallImage  className="CrystalBallImage"/>
 			<CrystalBallTextResponse />
 			<BackgroundImage />
 		</div>
