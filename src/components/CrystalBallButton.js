404e50d60c1132feccf70ded39f40055e4909236
Stephen Boynton <boyntonsc@gmail.com>
2017-10-01 14:53:40 -0400

    Link to fortune teller, animate buttons

--- src/components/CrystalBallButton.js
+++ src/components/CrystalBallButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/CrystalBallButton.css";
 
 export default function CrystalBallButton(props) {
 	return (
-		<button className="crystal-ball-btn" >
-			{props.name}
+		<button className="crystal-ball-btn">
+			<Link to="/fortune">{props.name}</Link>
 		</button>
-)
+	);
 }
