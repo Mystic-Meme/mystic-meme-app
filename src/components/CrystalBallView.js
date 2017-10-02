7756901f53cbf9153d16cda2f9ad62491acb1a73
Chad Yates <cayates1989@gmail.com>
2017-10-02 15:45:03 -0400

    fixed some typos

--- src/components/CrystalBallView.js
+++ src/components/CrystalBallView.js
@@ -7,9 +7,9 @@ import CrystalBallButton from "./CrystalBallButton"
 export default function CrystalBallView() {
 	return (
 		<div className="crystal-ball-view">
-			<h1>
+			<h1 className="typewriter">
 				You should know, not all is as it seems ... <br /> Now choose your
-				teller, and recieve you your meme ...
+				teller, and receive you your meme ...
 			</h1>
 			{/* <CrystalBallButton /> */}
 			{/* <CrystalBallButton /> */}
